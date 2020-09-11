49f05be45a6987f59ead4abe11c7d8f0901c5983
Duni03 <61987129+Duni03@users.noreply.github.com>
2020-09-11 20:48:38 +0530

    Update settings.py

--- instad/settings.py
+++ instad/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -121,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 
