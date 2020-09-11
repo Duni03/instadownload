b3359d15e707d895f65c1b28add10a38599ded3d
Duni03 <61987129+Duni03@users.noreply.github.com>
2020-09-11 22:41:38 +0530

    Update urls.py

--- down/urls.py
+++ down/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path("", views.index, name="index"),
+    path("down",views.two),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
